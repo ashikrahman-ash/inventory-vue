ad52da10bcffd452cbd59c973fbe649bcae7cc4e
Ashik <ashikur1993@gmail.com>
2022-12-30 19:21:47 +0600

    Services section adding

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,78 @@
     <div>
         <Header />
         <HomeHero />
-        <h2 class="">Home page</h2>
 
+        <section class="services-wrapper pb-20">
+            <div class="container mx-auto py-[100px] px-40 mt-[-200px] bg-white rounded-[20px]">
+                <div class="services-box grid grid-cols-3 gap-[30px] ">
+                    <div class="service-item pt-10 px-8 pb-8 bg-dark7 rounded-xl border-2 border-dark7 hover:border-dark5 hover:bg-white transition-colors duration-200">
+                        <div class="service-icon mb-8">
+                            <img src="../assets/img/services-icon-1.png" alt="" class=" max-w-full">
+                        </div>
+                        <h3 class="service-heading font-semibold text-dark1 text-4xl mb-5">
+                            Inventory made Easy
+                        </h3>
+                        <p class=" text-dark2 text-lg">
+                            Track products from inventory till fulfillment so that nothing falls thru the cracks</p>
+                    </div>
+                    <div class="service-item pt-10 px-8 pb-8 bg-dark7 rounded-xl border-2 border-dark7 hover:border-dark5 hover:bg-white transition-colors duration-200">
+                        <div class="service-icon mb-8">
+                            <img src="../assets/img/services-icon-2.png" alt="" class=" max-w-full">
+                        </div>
+                        <h3 class="service-heading font-semibold text-dark1 text-4xl mb-5">
+                            Single Platform
+                        </h3>
+                        <p class=" text-dark2 text-lg">
+                            Manage your inventory from purchasing to accounting
+                        </p>
+                    </div>
+                    <div class="service-item pt-10 px-8 pb-8 bg-dark7 rounded-xl border-2 border-dark7 hover:border-dark5 hover:bg-white transition-colors duration-200">
+                        <div class="service-icon mb-8">
+                            <img src="../assets/img/services-icon-3.png" alt="" class=" max-w-full">
+                        </div>
+                        <h3 class="service-heading font-semibold text-dark1 text-4xl mb-5">
+                            Think Ahead
+                        </h3>
+                        <p class=" text-dark2 text-lg">
+                            Predict reordering based on demand, season and most profitable items.
+                        </p>
+                    </div>
+                    <div class="service-item pt-10 px-8 pb-8 bg-dark7 rounded-xl border-2 border-dark7 hover:border-dark5 hover:bg-white transition-colors duration-200">
+                        <div class="service-icon mb-8">
+                            <img src="../assets/img/services-icon-4.png" alt="" class=" max-w-full">
+                        </div>
+                        <h3 class="service-heading font-semibold text-dark1 text-4xl mb-5">
+                            Be in Control
+                        </h3>
+                        <p class=" text-dark2 text-lg">
+                            Track products from inventory till fulfillment so that nothing falls thru the cracks
+                        </p>
+                    </div>
+                    <div class="service-item pt-10 px-8 pb-8 bg-dark7 rounded-xl border-2 border-dark7 hover:border-dark5 hover:bg-white transition-colors duration-200">
+                        <div class="service-icon mb-8">
+                            <img src="../assets/img/services-icon-5.png" alt="" class=" max-w-full">
+                        </div>
+                        <h3 class="service-heading font-semibold text-dark1 text-4xl mb-5">
+                            User Friendly
+                        </h3>
+                        <p class=" text-dark2 text-lg">
+                            Track products from inventory till fulfillment so that nothing falls thru the cracks
+                        </p>
+                    </div>
+                    <div class="service-item pt-10 px-8 pb-8 bg-dark7 rounded-xl border-2 border-dark7 hover:border-dark5 hover:bg-white transition-colors duration-200">
+                        <div class="service-icon mb-8">
+                            <img src="../assets/img/services-icon-6.png" alt="" class=" max-w-full">
+                        </div>
+                        <h3 class="service-heading font-semibold text-dark1 text-4xl mb-5">
+                            Mobile Friendly
+                        </h3>
+                        <p class=" text-dark2 text-lg">
+                            Track products from inventory till fulfillment so that nothing falls thru the cracks
+                        </p>
+                    </div>
+                </div>
+            </div>        
+        </section>
     </div>
 </template>
 
