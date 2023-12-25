3c3486ca44c4cb83f73944e85b6ef9d7e0df9ec6
Ashik <ashikur1993@gmail.com>
2023-12-25 14:55:09 +0600

    Update

--- components/SecondaryBtn.vue
+++ components/SecondaryBtn.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        <NuxtLink to="/" class="text-base md:text-lg py-3 md:py-[17px] px-7 bg-secondary hover:bg-main_purple transition-all duration-300 hover:text-white ease-in-out text-dark1 font-semibold inline-flex rounded-lg h-[50px] md:h-[58px] items-center min-w-[205px] justify-center">
+        <NuxtLink to="/"
+            class="text-base md:text-lg py-3 md:py-[17px] px-7 bg-secondary hover:bg-main_purple transition-all duration-300 hover:text-white ease-in-out text-dark1 font-semibold flex rounded-lg h-[50px] md:h-[58px] items-center min-w-[205px] justify-center">
             <slot />
         </NuxtLink>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,68 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./components/**/*.{js,vue,ts}",
-		"./layouts/**/*.vue",
-		"./pages/**/*.vue",
-		"./plugins/**/*.{js,ts}",
-		"./nuxt.config.{js,ts}",
-		"./app.vue",
-		"./error.vue",
-	],
+    content: [
+        "./components/**/*.{js,vue,ts}",
+        "./layouts/**/*.vue",
+        "./pages/**/*.vue",
+        "./plugins/**/*.{js,ts}",
+        "./nuxt.config.{js,ts}",
+        "./app.vue",
+        "./error.vue",
+    ],
     theme: {
-		
-		fontFamily: {
-			roobert: "'Roobert', sans",
-		},
+        fontFamily: {
+            roobert: "'Roobert', sans",
+        },
 
-		container: {
+        container: {
             center: true,
         },
 
+        colors: {
+            //Primary colors
+            main_purple: "#2E00FF",
+            main_dark: "#0B0140",
+            main_light: "#F5F3FD",
 
-		colors: {
-			//Primary colors
-			main_purple: "#2E00FF",
-			main_dark: "#0B0140",
-			main_light: "#F5F3FD",
-	
-			//Secondary Color
-			secondary: "#4AF8E3",
-			secondary_shade: "#FFF3EE",
-			secondary_light: "#F3EADA",
-	
-			//Dark Color
-			dark1: "#06021C",
-			dark2: "#33313E",
-			dark3: "#5D5B70",
-			dark4: "#BAB8CF",
-			dark5: "#E6E5ED",
-			dark6: "#F4F2F7",
-			dark7: "#F9F8FC",
-	
-			//Status Color
-			info_color: "#219FFF",
-			info_shade: "#E9F6FF",
-	
-			success_color: "#17BD8D",
-			success_shade: "#E9FBF6",
-	
-			warning_color: "#FFA114",
-			warning_shade: "#FFF7EB",
-	
-			danger_color: "#FF4E3E",
-			danger_shade: "#FEF4F3",
-	
-			//Base colors
-			white: "#FFFFFF",
-			black: "#000000",
+            //Secondary Color
+            secondary: "#4AF8E3",
+            secondary_shade: "#FFF3EE",
+            secondary_light: "#F3EADA",
 
-			dark_shade: "#635C88",
-		},
-		
+            //Dark Color
+            dark1: "#06021C",
+            dark2: "#33313E",
+            dark3: "#5D5B70",
+            dark4: "#BAB8CF",
+            dark5: "#E6E5ED",
+            dark6: "#F4F2F7",
+            dark7: "#F9F8FC",
+
+            //Status Color
+            info_color: "#219FFF",
+            info_shade: "#E9F6FF",
+
+            success_color: "#17BD8D",
+            success_shade: "#E9FBF6",
+
+            warning_color: "#FFA114",
+            warning_shade: "#FFF7EB",
+
+            danger_color: "#FF4E3E",
+            danger_shade: "#FEF4F3",
+
+            //Base colors
+            white: "#FFFFFF",
+            black: "#000000",
+
+            dark_shade: "#635C88",
+        },
 
         extend: {
-			
-
-			
-		},
+            screens: {
+                xsm: "450px",
+            },
+        },
     },
     plugins: [],
 };
